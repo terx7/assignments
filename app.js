3884a7925dc903337e048ad33cf533391f1674f7
terx7 <55434235+terx7@users.noreply.github.com>
2020-04-22 16:33:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const textBoxDiv = document.getElementById('text-box');
 const typingArea = document.getElementById('typing-area');
 const feedbackDiv = document.getElementById('feedback');
+const timerBoxDiv = document.getElementById('timer');
 
 
 
 const text = "Peetrus tuli külla";
+let wordsArray = text.split(' ')
 textBoxDiv.innerText = text;
 let wordCount = 0
 highlight()
@@ -12,21 +14,38 @@ highlight()
 let errorCount = 0
 feedbackDiv.innerText = errorCount
 
+// Timer
+let timer = 0
+timerBoxDiv.innerText = '0:00'
+
+setInterval(() => {
+    timer++
+    minutes = parseInt(timer / 60)
+    seconds = (timer % 60).toString().padStart(2, '0')
+    timerBoxDiv.innerText = minutes + ':' + seconds
+}, 1000);
+
+// Keydown event
 typingArea.focus()
 typingArea.addEventListener('keydown', event => {
     console.log(typingArea.value)
+
     if (event.keyCode == 32) {
         if(text.split(' ')[wordCount] != typingArea.value.split(' ')[wordCount] ) {
+            wordsArray[wordCount] = '<span class="incorrect">' + wordsArray[wordCount] + '</span>'
             errorCount++
             feedbackDiv.innerText = errorCount
+        } else {
+            wordsArray[wordCount] = '<span class="correct">' + wordsArray[wordCount] + '</span>'
         }
+
         wordCount++
         highlight()
     }
 })
 
 function highlight () {
-    let wordsArray = text.split(' ')
-    wordsArray[wordCount] = '<span class="highlight">' + wordsArray[wordCount] + '</span>'
-    textBoxDiv.innerHTML = wordsArray.join(' ')
+    let highlightedArray = Array.from(wordsArray)
+    highlightedArray[wordCount] = '<span class="highlight">' + highlightedArray[wordCount] + '</span>'
+    textBoxDiv.innerHTML = highlightedArray.join(' ')
 }
